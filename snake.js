4050cfda490cb70f250576fb68946781b297435d
cristina.marques <cristina.marques@infosupport.com>
2023-09-12 11:13:06 +0200

    restart game by pressing a key

--- snake.js
+++ snake.js
@@ -5,24 +5,28 @@ const snake_border = "#246d9e";
 const food_color = "#bbff96";
 const food_border = "#44783d";
 
-
-let snake = [
+const START_SNAKE = [
   { x: 200, y: 200 },
   { x: 190, y: 200 },
   { x: 180, y: 200 },
-  { x: 170, y: 200 },
-  { x: 160, y: 200 },
+  { x: 170, y: 200 }
 ];
 
+let snake = [...START_SNAKE];
 let score = 0;
-let game_active = false;
+const GAME_STATES = {
+  START: 0,
+  PLAYING: 1,
+  GAME_OVER: 2,
+}
+let game_state = GAME_STATES.START;
 let changing_direction = false;
 let food_x;
 let food_y;
 
 // Snake direction
-const speed = 10;
-let dx = speed;
+const SPEED = 10;
+let dx = SPEED;
 let dy = 0;
 
 
@@ -37,21 +41,12 @@ document.addEventListener("keydown", handle_keyPress);
 
 function main() {
   if (has_game_ended()) {
-    // Add red border to snakeboard
-    snakeboard_ctx.strokeStyle = '#ff3f2e';
-    snakeboard_ctx.strokeRect(0, 0, snakeboard.width, snakeboard.height);
-
-    // Draw red square where snake died
-    snakeboard_ctx.fillStyle = '#ff3f2e';
-    snakeboard_ctx.fillRect(snake[0].x, snake[0].y, 10, 10);
-    snakeboard_ctx.strokeStyle = '#b01e3e';
-    snakeboard_ctx.strokeRect(snake[0].x, snake[0].y, 10, 10);
-
-
-    document.getElementById("score").innerHTML = "Game Over!<br>Score: " + score;
-    
+    game_over();
+    return;
+  } else if(game_state === GAME_STATES.START) {
     return;
   }
+
   changing_direction = false;
   
   setTimeout(function onTick() {
@@ -61,9 +56,8 @@ function main() {
     drawSnake();
     main();
   }, 100);
-
-  
 }
+
 function drawRectWithBorder(x, y, width, height, color, border) {
   snakeboard_ctx.fillStyle = color;
   snakeboard_ctx.strokeStyle = border;
@@ -89,14 +83,11 @@ function drawSnakePart(snakePart) {
 }
 
 function has_game_ended() {
+  // Snake hits itself
   for (let i = 4; i < snake.length; i++) {
     if (snake[i].x === snake[0].x && snake[i].y === snake[0].y) return true;
   }
-  const hitLeftWall = snake[0].x < 0;
-  const hitRightWall = snake[0].x > snakeboard.width - 10;
-  const hitToptWall = snake[0].y < 0;
-  const hitBottomWall = snake[0].y > snakeboard.height - 10;
-  return hitLeftWall || hitRightWall || hitToptWall || hitBottomWall;
+  return false;
 }
 
 function random_food(min, max) {
@@ -104,58 +95,86 @@ function random_food(min, max) {
 }
 
 function gen_food() {
-  // Generate a random number the food x-coordinate
   food_x = random_food(0, snakeboard.width - 10);
-  // Generate a random number for the food y-coordinate
   food_y = random_food(0, snakeboard.height - 10);
   // if the new food location is where the snake currently is, generate a new food location
-  snake.forEach(function has_snake_eaten_food(part) {
-    const has_eaten = part.x == food_x && part.y == food_y;
-    if (has_eaten) gen_food();
-  });
+  if (snake.some(part => part.x === food_x && part.y === food_y)) gen_food();
 }
 
 function handle_keyPress(event) {
-  if(!game_active) {
-    game_active = true;
+  if(game_state === GAME_STATES.START) {
+    game_state = GAME_STATES.PLAYING;
     document.getElementById("score").innerHTML = score;
     main();
-  }
+  } else if(game_state === GAME_STATES.GAME_OVER) {
+    game_state = GAME_STATES.START;
+    game_restart();
+  } else {
 
-  const LEFT_KEY = 37;
-  const RIGHT_KEY = 39;
-  const UP_KEY = 38;
-  const DOWN_KEY = 40;
-  const W_KEY = 87;
-  const A_KEY = 65;
-  const S_KEY = 83;
-  const D_KEY = 68;
+    const LEFT_KEY = 37;
+    const RIGHT_KEY = 39;
+    const UP_KEY = 38;
+    const DOWN_KEY = 40;
+    const W_KEY = 87;
+    const A_KEY = 65;
+    const S_KEY = 83;
+    const D_KEY = 68;
 
-  if (changing_direction) return;
-  changing_direction = true;
+    if (changing_direction) return;
+    changing_direction = true;
 
-  const keyPressed = event.keyCode;
-  const goingUp = dy === -10;
-  const goingDown = dy === 10;
-  const goingRight = dx === 10;
-  const goingLeft = dx === -10;
-  if (keyPressed === (A_KEY || LEFT_KEY) && !goingRight) {
-    dx = -speed
-    dy = 0;
-  }
-  if (keyPressed === (W_KEY || UP_KEY) && !goingDown) {
-    dx = 0;
-    dy = -speed;
-  }
-  if (keyPressed === (D_KEY || RIGHT_KEY) && !goingLeft) {
-    dx = speed;
-    dy = 0;
-  }
-  if (keyPressed === (S_KEY || DOWN_KEY) && !goingUp) {
-    dx = 0;
-    dy = speed;
+    const keyPressed = event.keyCode;
+    const goingUp = dy === -SPEED;
+    const goingDown = dy === SPEED;
+    const goingRight = dx === SPEED;
+    const goingLeft = dx === -SPEED;
+    if (keyPressed === (A_KEY || LEFT_KEY) && !goingRight) {
+      dx = -SPEED
+      dy = 0;
+    }
+    if (keyPressed === (W_KEY || UP_KEY) && !goingDown) {
+      dx = 0;
+      dy = -SPEED;
+    }
+    if (keyPressed === (D_KEY || RIGHT_KEY) && !goingLeft) {
+      dx = SPEED;
+      dy = 0;
+    }
+    if (keyPressed === (S_KEY || DOWN_KEY) && !goingUp) {
+      dx = 0;
+      dy = SPEED;
+    }
   }
 }
+
+function game_restart() {
+  snake = [...START_SNAKE];
+  score = 0;
+  dx = SPEED;
+  dy = 0;
+  clear_board();
+  gen_food();
+  drawFood();
+  drawSnake();
+  document.getElementById("score").innerHTML = "Press any key to start";
+}
+
+function game_over() {
+  game_state = GAME_STATES.GAME_OVER;
+
+  // Add red border to snakeboard
+  snakeboard_ctx.strokeStyle = '#ff3f2e';
+  snakeboard_ctx.strokeRect(0, 0, snakeboard.width, snakeboard.height);
+
+  // Draw red square where snake died
+  snakeboard_ctx.fillStyle = '#ff3f2e';
+  snakeboard_ctx.fillRect(snake[0].x, snake[0].y, 10, 10);
+  snakeboard_ctx.strokeStyle = '#b01e3e';
+  snakeboard_ctx.strokeRect(snake[0].x, snake[0].y, 10, 10);
+
+  document.getElementById("score").innerHTML = "Game Over! Score: " + score + "<br>Press any key to restart";
+}
+
 
 function move_snake() {
   const head = { x: snake[0].x + dx, y: snake[0].y + dy };
